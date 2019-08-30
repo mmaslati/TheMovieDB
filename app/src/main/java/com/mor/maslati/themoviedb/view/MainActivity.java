b63d404a3c26309e4bd90f058d01e994bc9200e6
Mor Maslati <mor.maslati@gmail.com>
2019-08-30 18:47:21 +0300

    Starting to setup project.

--- app/src/main/java/com/mor/maslati/themoviedb/view/MainActivity.java
+++ app/src/main/java/com/mor/maslati/themoviedb/view/MainActivity.java
@@ -1,14 +1,17 @@
-package com.mor.maslati.themoviedb;
+package com.mor.maslati.themoviedb.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.mor.maslati.themoviedb.R;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
